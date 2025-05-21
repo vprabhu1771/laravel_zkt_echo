7361ba87b6a2d82d3b6baafee87bc8cfe411d03b
vprabhu1771 <vprabhu1771@gmail.com>
2025-05-21 09:39:51 +0530

    HomeController api v1 routes added

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+use App\Http\Controllers\api\v1\HomeController;
+
+Route::get('/version', [HomeController::class, 'getVersion'])->name('getVersion');
+
+Route::get('/platform', [HomeController::class, 'getPlatform'])->name('getPlatform');
+
+Route::get('/test_voice', [HomeController::class, 'testVoice'])->name('testVoice');
+
+Route::get('/device_name', [HomeController::class, 'getDeviceName'])->name('getDeviceName');
+
+Route::get('/shutdown', [HomeController::class, 'shutdown'])->name('shutdown');
+
+Route::get('/restart', [HomeController::class, 'restart'])->name('restart');
+
+Route::get('/sleep', [HomeController::class, 'sleep'])->name('sleep');
+
+Route::get('/resume', [HomeController::class, 'resume'])->name('resume');
+
+Route::get('/zkteco/roll_no', [HomeController::class, 'getRollNo']);
+
+Route::post('/zkteco/users', [HomeController::class, 'getUsersById']);
